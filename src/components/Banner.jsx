f8e38315e333604236bfeffb04ea4880c3267986
facchin21 <ferminfacchinq@gmail.com>
2024-05-13 11:14:35 -0300

    Agrego menu funcional

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -30,13 +30,14 @@ const mostrarMenu = () => {
 export const Banner = () => {
   return (
     <div className="banner__container">
-        <header className='flex justify-between px-8 py-4 items-center'>
+        <header className='flex justify-between px-6 py-4'>
             <div>
                 <h1 className='font-bold text-3xl
                 text-white font-barlow'>{title}
                 </h1>
             </div>
-            <div onClick={mostrarMenu}>
+            <div onClick={mostrarMenu} className='
+            flex flex-col items-end cursor-pointer'>
                 {/* Icono menu */}
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -44,11 +45,18 @@ export const Banner = () => {
                     <path d="M4 12l16 0" />
                     <path d="M4 18l16 0" />
                 </svg>
-            </div>
-            <div className='menu__container'>
-                <ul>
-                    {menuItems.map((item, index)=>{})}
+            <div className='menu__container pt-8 mr-2'>
+                <ul className='bg-white py-5 p-12'>
+                    {menuItems.map((item, index) => (
+                        <li key={index} className='py-3 px-6 my-4
+                        font-barlow text-center text-veryDarkGrayishBlue2
+                        font-semibold transition-all duration-300
+                        hover:text-black hover:bg-yellow rounded-3xl'>
+                             <a href={item.link}>{item.title}</a>   
+                         </li>
+                    ))}
                 </ul>
+            </div>
             </div>
         </header>
 
